9ac055c6de2227f16174ca501bda966bfcfa6487
Caramel1004 <Caramel1004.js@gmail.com>
2023-04-18 15:52:53 +0900

    유저 스키마 구축

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// 유저 스키마
+//이메일, 이름, 비밀번호, 상태, 게시물
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            reference: 'Post'
+        }
+    ],
+    createdAt: {
+        type: Date
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
